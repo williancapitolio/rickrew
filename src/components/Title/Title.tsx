8c31e0f69ae1f431bc9d63dd543d36f4e1a9cf21
Willian <williansilvap25@gmail.com>
2023-10-18 00:06:14 -0300

    rename wrapper to component name on styles

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -3,9 +3,9 @@ import styles from "./Title.module.scss";
 export const Title = () => {
   return (
     <>
-      <h1 className={styles.wrapper}>
-        <span className={styles.wrapperSpan}>R</span>ic
-        <span className={styles.wrapperSpan}>K</span>rew
+      <h1 className={styles.title}>
+        <span className={styles.titleSpan}>R</span>ic
+        <span className={styles.titleSpan}>K</span>rew
       </h1>
     </>
   );

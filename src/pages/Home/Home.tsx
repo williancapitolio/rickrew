75a445f634c79256233cf85d275cd3fc79f1f106
Willian <williansilvap25@gmail.com>
2023-10-18 23:43:38 -0300

    walking on infinite scroll...

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,12 +8,34 @@ import {
   CharacterType,
   ResponseCharactersType,
 } from "../../types/CharactersType";
+import { getAllCharacters } from "../../services/Api";
 
 export const Home = () => {
   const [data, setData] = useState<[] | CharacterType[]>([]);
   const [loading, setLoadind] = useState(false);
+  const [scrollLoadind, setScrollLoadind] = useState(false);
+  const [page, setPage] = useState(2);
+  const [isEndOfPage, setisEndOfPage] = useState(false);
 
   const loadedData = useLoaderData() as ResponseCharactersType;
+
+  function getEndOfPage() {
+    const scrollPosition = window.scrollY + window.innerHeight;
+    const documentHeight = document.documentElement.offsetHeight;
+    scrollPosition >= documentHeight - 1000 && setisEndOfPage(true);
+  }
+
+  const fetchNextCharacters = async () => {
+    if (page > loadedData.info.pages) return;
+    setScrollLoadind(true);
+
+    setPage(page + 1);
+
+    const nextCharacters = await getAllCharacters(page);
+
+    setData([...data, ...nextCharacters.results]);
+    setScrollLoadind(false);
+  };
 
   useEffect(() => {
     setLoadind(true);
@@ -22,20 +44,24 @@ export const Home = () => {
   }, [loadedData]);
 
   window.addEventListener("scroll", () => {
-    //scroll - tam janela - tam inteiro pagina - final da pagina 500??
-    //console.log(window.scrollY, window.innerHeight, document.documentElement.offsetHeight, window.scrollY + window.innerHeight)
-    //chamar funcao que poe loadingpagination como true
-    if (window.scrollY + window.innerHeight === 6715) {
-      setData([...data, loadedData.results])
+    getEndOfPage();
+    if (isEndOfPage) {
+      setTimeout(() => {
+        fetchNextCharacters();
+        setisEndOfPage(false);
+      }, 1000 * 2);
     }
-  })
+  });
 
   return (
-    <section>
-      {loading && <h2>Carregando...</h2>}
-      {data.map((character) => (
-        <CharacterCard characterData={character} key={character.id} />
-      ))}
-    </section>
+    <>
+      <section>
+        {loading && <h2>Carregando...</h2>}
+        {data.map((character) => (
+          <CharacterCard characterData={character} key={character.id} />
+        ))}
+      </section>
+      {scrollLoadind && <h2>Carregando...</h2>}
+    </>
   );
 };

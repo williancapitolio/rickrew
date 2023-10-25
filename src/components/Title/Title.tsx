7f87dcfcf74e1e366c19deacc96339c121cd090e
Willian <williansilvap25@gmail.com>
2023-10-25 00:13:21 -0300

    add navigate on title page

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,12 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 import styles from "./Title.module.scss";
 
 export const Title = () => {
+  const navigate = useNavigate();
+
   return (
-    <>
-      <h1 className={styles.title}>
-        <span className={styles.titleSpan}>R</span>ic
-        <span className={styles.titleSpan}>K</span>rew
-      </h1>
-    </>
+    <h1 className={styles.title} onClick={() => navigate("/")}>
+      <span className={styles.titleSpan}>R</span>ic
+      <span className={styles.titleSpan}>K</span>rew
+    </h1>
   );
 };

035d70a5b3f4a20219ad7ec7ef24a452fc36c68a
Willian <williansilvap25@gmail.com>
2023-10-27 23:30:19 -0300

    add hook to get favorites characters list

---
+++ src/hooks/useFavoritesCharactersList.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+import { useLoaderData } from "react-router-dom";
+
+import { CharacterType } from "../types/CharactersType";
+
+export const useFavoritesCharactersList = () => {
+  const [data, setData] = useState<[] | CharacterType[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const loadedData = useLoaderData() as CharacterType[];
+
+  useEffect(() => {
+    setLoading(true);
+    setData(loadedData);
+    setLoading(false);
+  }, [loadedData]);
+
+  return {data, loading}
+};
--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,13 +1,21 @@
 import { useLoaderData } from "react-router-dom";
 
+import { BackHome } from "../../components/BackHome";
+import { Subtitle } from "../../components/Subtitle";
+
+import styles from "./Favorites.module.scss";
+
 import { CharacterType } from "../../types/CharactersType";
 
 export const Favorites = () => {
-  const data = useLoaderData() as CharacterType[];
-  console.log(data)
+  const loadedData = useLoaderData() as CharacterType[];
+  console.log(loadedData);
   return (
-    <>
+    <section className={styles.favorites}>
+      <BackHome />
+      <Subtitle />
+
       <div></div>
-    </>
+    </section>
   );
 };

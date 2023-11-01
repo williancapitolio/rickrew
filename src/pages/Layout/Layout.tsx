11457d38cb7a6b160ecb74bc46a258d1ffd578fb
Willian <williansilvap25@gmail.com>
2023-10-31 22:47:01 -0300

    add scroll to top function

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
 
+import { useScrollToTop } from "../../hooks/useScrollToTop";
+
 import styles from "./Layout.module.scss";
 
 export const Layout = () => {
+  useScrollToTop();
+
   return (
     <main className={styles.layout}>
       <Outlet />
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -33,7 +33,7 @@ export const router = createBrowserRouter([
         path: "favorites",
         element: <Favorites />,
         loader: favoritesLoader,
-        errorElement: <FavoritesError  />,
+        errorElement: <FavoritesError />,
       },
     ],
   },

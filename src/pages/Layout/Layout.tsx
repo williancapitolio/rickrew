373e1bbfd861fd564ea8342cc52c7b5eb19683a6
Willian <williansilvap25@gmail.com>
2023-11-03 00:20:18 -0300

    remove scroll to top hook and add it on layout page

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,11 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
 
-import { useScrollToTop } from "../../hooks/useScrollToTop";
+import { Outlet, useLocation } from "react-router-dom";
 
 import styles from "./Layout.module.scss";
 
 export const Layout = () => {
-  useScrollToTop();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "instant" });
+  }, [pathname]);
 
   return (
     <main className={styles.layout}>
